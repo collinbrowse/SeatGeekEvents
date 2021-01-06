24c33d48ea12d25f3be367fad2c21670870e9ca5
Collin Browse <cbrowse13@gmail.com>
2021-01-06 12:09:25 -0700

    Initial Commit

---
+++ FetchRewardsCodingExercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FetchRewardsCodingExercise
+//
+//  Created by Collin Browse on 1/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
