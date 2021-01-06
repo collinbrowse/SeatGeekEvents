fc8e4a39ef81e7ff3032e553d7c06404219256d4
Collin Browse <cbrowse13@gmail.com>
2021-01-06 12:25:22 -0700

    Added initial setup. There will be one branch for a solo build on a test project. Typically branches are used in the format of: enhance, feature, improve, bug

---
+++ FetchRewardsCodingExercise/Screens/HomeScreenVC.swift
@@ -0,0 +1,21 @@
+//
+//  HomeScreenVC.swift
+//  FetchRewardsCodingExercise
+//
+//  Created by Collin Browse on 1/6/21.
+//
+
+import UIKit
+
+class HomeScreenViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        layoutUI()
+    }
+    
+    
+    private func layoutUI() {
+        view.backgroundColor = .systemRed
+    }
+}
